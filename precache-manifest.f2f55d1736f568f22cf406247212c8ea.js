8bb69869e22fd562f42cb91ca3d37f10e00ea9fb
gianlopes <gianlucas1020@hotmail.com>
2020-03-17 21:56:45 -0300

    Updates

---
+++ precache-manifest.f2f55d1736f568f22cf406247212c8ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a277a07decb980c3abb7a899358255c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5c44215d3c340e81c119",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "27bfd9215171890aaef1",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5c44215d3c340e81c119",
+    "url": "/robofriends/static/js/2.32d5b9d8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.32d5b9d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bfd9215171890aaef1",
+    "url": "/robofriends/static/js/main.5d109043.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  }
+]);
