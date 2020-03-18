f5347dbc05e239d3c8dacaf41e8806f60268eeda
gianlopes <gianlucas1020@hotmail.com>
2020-03-17 21:08:33 -0300

    Implement card component

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-const Card = () => {
-    return (
-        <div>
-            <img src='' alt='robots'></img>
-            <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
-            </div>
-        </div>
-    );
+const Card = ({ name, email,id }) => {
+  return (
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img src={`https://robohash.org/${id}?200x200`} alt='robots'></img>
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
-
